2f08082d3c7763318f895889e40415e79a3e75fc
Ryan Albon <ryanalbon@gmail.com>
2019-10-27 18:33:30 +0100

    Refactoring state management into dedicated container.

--- src/App.js
+++ src/App.js
@@ -3,24 +3,37 @@ import AppContext from './AppContext';
 import WidgetForm from './WidgetForm';
 import WidgetList from './WidgetList';
 
+function createStore() {
+  let callback;
+  let state = {
+    widgets: [
+      'Alice',
+      'Bob',
+    ],
+    addWidget: (widget) => {
+      state = {
+        ...state,
+        widgets: [ ...state.widgets, widget ],
+      };
+      callback(state);
+    },
+  };
+
+  return {
+    getState: () => state,
+    setCallback: cb => callback = cb,
+  }
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      widgets: [
-        'Alice',
-        'Bob',
-      ],
-      addWidget: (widget) => {
-        this.setState(
-          function (currentState) {
-            return {
-              widgets: [ ...currentState.widgets, widget ],
-            };
-          }
-        );
-      },
-    };
+
+    const store = createStore();
+
+    store.setCallback(state => this.setState({ ...state }));
+
+    this.state = { ...store.getState() };
   }
 
   render() {
